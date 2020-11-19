7a2a79ede774d345b66abada4e1e76abbf38ef5e
Sammy Samkough <sammysamkough@gmail.com>
2020-11-19 15:16:32 -0500

    updated scraper to fix desc

--- njCovidStats.py
+++ njCovidStats.py
@@ -5,13 +5,14 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import datetime
+import re
 import csv
 
 
 def getNJCovidStats():
     driver = webdriver.Chrome()
     url = "https://maps.arcgis.com/apps/opsdashboard/index.html#/ec4bffd48f7e495182226eee7962b422"
+    # url = "https://maps.arcgis.com/apps/MapSeries/index.html?appid=c2efd1898e48452e83d7218329e953d7"
     driver.get(url)
 
     timeout = 10
@@ -27,23 +28,39 @@ def getNJCovidStats():
 
     htmlPage = driver.page_source
 
-    # get all cases
     soup = BeautifulSoup(htmlPage, 'html.parser')
     featureList = soup.find_all("nav", class_="feature-list")
-    featureList = featureList[1]
-    featureList = "<html>" + str(featureList) + "</html>"
+
+    # get date
+    dateFeatureList = featureList[1]
+    dateFeatureList = "<html>" + str(dateFeatureList) + "</html>"
+
+    soup = BeautifulSoup(dateFeatureList, 'html.parser')
+    innerDivDate = soup.find("div", class_="external-html")
+    innerDivDate = "<html>" + str(innerDivDate) + "</html>"
+
+    soup = BeautifulSoup(innerDivDate, 'html.parser')
+    currDate = soup.find("strong")
+    currDate = currDate.text[9:]
+
+    # get cases
+    caseFeatureList = featureList[2]
+    caseFeatureList = "<html>" + str(caseFeatureList) + "</html>"
 
     countyObjList = []
     countyObj = {}
-    soup = BeautifulSoup(featureList, 'html.parser')
+    soup = BeautifulSoup(caseFeatureList, 'html.parser')
 
     for emberElem in soup.find_all("span", class_=["flex-horizontal", "feature-list-item", "ember-view"]):
         feature = "<html>" + str(emberElem) + "</html>"
         soup = BeautifulSoup(feature, 'html.parser')
         strongElems = soup.find_all("strong")
 
-        countyObj["name"] = strongElems[0].text.replace(u" County", u"")
+        countyObj["name"] = strongElems[0].text.replace(" County", "")
         countyObj["cases"] = strongElems[3].text.replace(u"\xa0", u"")
+        countyObj["cases"] = countyObj["cases"].replace(u"\"", u"")
+        countyObj["cases"] = countyObj["cases"].replace(u",", u"")
+        countyObj["cases"] = re.sub("\D", "", countyObj["cases"])
         countyObjList.append(countyObj)
         countyObj = {}
 
@@ -57,22 +74,19 @@ def getNJCovidStats():
     casesNoCounty = soup.find('text').text
 
     # get total cases
-    totalCasesContainer = "<html>" + str(svgArr[15]) + "</html>"
+    totalCasesContainer = "<html>" + str(svgArr[5]) + "</html>"
     soup = BeautifulSoup(totalCasesContainer, 'html.parser')
     totalCases = soup.find('text').text
 
     # get total deaths
-    totalDeathsContainer = "<html>" + str(svgArr[17]) + "</html>"
+    totalDeathsContainer = "<html>" + str(svgArr[7]) + "</html>"
     soup = BeautifulSoup(totalDeathsContainer, 'html.parser')
     totalDeaths = soup.find('text').text
 
     driver.quit()
 
     # get description
-    dt = datetime.datetime.today()
-    month_day = str(dt.year) + "/" + str(dt.month) + "/" + str(dt.day)
-
-    description = "As of " + month_day + " at 2pm, " + totalCases + \
+    description = "As of " + currDate + ", " + totalCases + \
         " people have been confirmed to be tested positive in New Jersey across all 21 counties. There have been " + \
         totalDeaths + " confirmed deaths."
     print(description)
